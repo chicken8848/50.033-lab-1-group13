6bf326dafea233fae136c95cf50bf90b294caa30
Joshua Lee <chicken8848@hotmail.com>
2025-09-25 10:17:20 +0800

    added camera rotation and movement

--- Assets/Scripts/DiscoBackground.cs
+++ Assets/Scripts/DiscoBackground.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class DiscoBackground : MonoBehaviour
 {
+    public Transform mariotransform;
+    public Vector3 cameraOffset = new Vector3(0,0,-10);
     Camera cam;
+    Transform camTransform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         cam = GetComponent<Camera>(); 
+        camTransform = GetComponent<Transform>();
     }
 
     void FixedUpdate()
     {
         cam.backgroundColor = new Color(Random.value, Random.value, Random.value);
+        camTransform.position = mariotransform.position + cameraOffset;
+        camTransform.localRotation = mariotransform.localRotation;
     }
 }
